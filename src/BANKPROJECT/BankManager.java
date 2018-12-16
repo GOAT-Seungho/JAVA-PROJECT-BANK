b5c7b74f9d3248af4843f8b6e26000c9135818cb
K.Seungho <rhanfwlq980@hanyang.ac.kr>
2018-12-16 18:38:45 +0900

    Login / Join

--- src/BANKPROJECT/BankManager.java
+++ src/BANKPROJECT/BankManager.java
@@ -4,17 +4,21 @@ public class BankManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BankReader reader = new BankReader();
 		
-		BankAccount ordinary_account = new BankAccount(0);		
-		BankWriter ordinary_writer = new BankWriter("입출금 통장", ordinary_account);
+		Login login = new Login();
 		
-		BankAccount saving_account = new BankAccount(0);
-		BankWriter saving_writer = new BankWriter("적금 통장", saving_account);
 		
-		BankController controller = new BankController(reader, ordinary_account, ordinary_writer, saving_account, saving_writer);
-
-		controller.processTransactions();
+//		BankReader reader = new BankReader();
+//		
+//		BankAccount ordinary_account = new BankAccount(0);		
+//		BankWriter ordinary_writer = new BankWriter("입출금 통장", ordinary_account);
+//		
+//		BankAccount saving_account = new BankAccount(0);
+//		BankWriter saving_writer = new BankWriter("적금 통장", saving_account);
+//		
+//		BankController controller = new BankController(reader, ordinary_account, ordinary_writer, saving_account, saving_writer);
+//
+//		controller.processTransactions();
 	}
 
 }
---
+++ src/BANKPROJECT/Join.java
@@ -0,0 +1,88 @@
+package BANKPROJECT;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.*;
+
+public class Join extends JFrame implements ActionListener {
+	
+	JButton button_ok;
+	JButton button_cancel;
+	JTextField ID;
+	JTextField PW;
+	
+	public Join() {
+		
+		button_ok = new JButton("OK");
+		button_cancel = new JButton("Cancel");
+
+		setTitle("Join");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container c = getContentPane();
+		c.setBackground(Color.gray);
+		c.setLayout(new FlowLayout());
+		
+		ID = new JTextField(20);
+		PW = new JPasswordField(20);
+		
+		c.add(new JLabel("ID"));
+		c.add(ID);
+		c.add(new JLabel("PW"));
+		c.add(PW);
+		c.add(button_ok);
+		c.add(button_cancel);
+		
+		setSize(290, 150);
+		setVisible(true);
+		
+		button_ok.addActionListener(this);
+		button_cancel.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ie) {
+		if (ie.getSource().equals(button_ok)) {
+			String member_info = ID.getText() + "," + PW.getText() + "\n";
+			
+			File file = new File("test1.txt");
+			FileWriter writer = null;
+			
+			try {
+				writer = new FileWriter(file, true);
+				writer.write(member_info);
+				writer.flush();
+				JOptionPane.showMessageDialog(null, "Done!");
+				setVisible(false);
+				new Login();
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (writer != null) writer.close();
+				}
+				catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
